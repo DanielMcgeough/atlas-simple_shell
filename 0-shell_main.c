54d46e882c1c13601b41433080837be522d28363
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-03-26 15:49:14 +0000

    fixed tab and space formatting for c files.

--- 0-shell_main.c
+++ 0-shell_main.c
@@ -2,27 +2,27 @@
 
 int main ()
 {
-        char *buffer;
-        char **cmd_ln = NULL;
-        size_t buffsize = 4095;
-        ssize_t bytes_read;
+	char *buffer;
+	char **cmd_ln = NULL;
+	size_t buffsize = 4095;
+	ssize_t bytes_read;
 
-        buffer = (char *)malloc(sizeof(char) * buffsize);
-        if (buffer == NULL)
-                return (-1);
+	buffer = (char *)malloc(sizeof(char) * buffsize);
+		if (buffer == NULL)
+			return (-1);
 
-        while (1)
-        {
-                printf("$ ");
-                bytes_read = getline(&buffer, &buffsize, stdin);
-                if (bytes_read == 1)
-                        continue;
-                buffer[bytes_read - 1] = '\0';
-                if (strncmp(buffer, "exit", 4) == 0)
-                        break;
-                cmd_ln = tokenize(buffer, " ");
-                forkcecute(cmd_ln);
-        }
-        free(buffer);
-        return (0);
+		while (1)
+		{
+			printf("$ ");
+			bytes_read = getline(&buffer, &buffsize, stdin);
+				if (bytes_read == 1)
+					continue;
+				buffer[bytes_read - 1] = '\0';
+				if (strncmp(buffer, "exit", 4) == 0)
+					break;
+				cmd_ln = tokenize(buffer, " ");
+				forkcecute(cmd_ln);
+		}
+		free(buffer);
+		return (0);
 }
