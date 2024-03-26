565dab1a201f1f4e8381df149b0c1639cb7d1850
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-26 14:37:09 +0000

    Forkcecute added from personal repository Exercises.

---
+++ 2-forkcecute.c
@@ -0,0 +1,41 @@
+#include "main_shell.h"
+
+int forkcecute(char **cmd_ln)
+{
+        pid_t pid;
+        /* char *args[]= {"ls", "-l", "/tmp", NULL};*/
+        char *envp[] = {NULL};
+
+        /* Check for empty arg array */
+        if (cmd_ln == NULL || cmd_ln[0] == NULL)
+        {
+                perror("forkcecute cmd_ln empty");
+                return (-1);
+        }
+
+        pid = fork();
+        if (pid == -1)
+        {
+                perror("fork failed");
+                return (-1);
+        }
+        else if (pid == 0)
+        {
+                /* Child process */
+                printf("Child process %d executing %s\n", getpid(),cmd_ln[0]);
+                if (execve(cmd_ln[0], cmd_ln, envp) == -1)
+                {
+                        perror("execve failed");
+                        exit(EXIT_FAILURE);
+                }
+                /* exit(EXIT_SUCCESS); */
+        }
+        else
+        {
+                /* Parent process */
+                /* Wait for the child to exit */
+                wait(NULL);
+                printf("Child process %d exited\n", pid);
+        }
+        return 0;
+}
