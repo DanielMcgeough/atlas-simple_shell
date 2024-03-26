5af34fcda0292c4858eca26e72fc8cde506c0d64
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-26 14:33:01 +0000

    Tokenize function from personal exercises repository.

---
+++ 1-tokenize.c
@@ -0,0 +1,41 @@
+#include "main_shell.h"
+
+char **tokenize(char *buffer, char *delimiter)
+{
+        int str_ct = 0, i = 0, l = 0;
+        unsigned long int k;
+        char **array = NULL;
+        char *portion;
+
+        for (k = 0; k < strlen(buffer); k++)
+        {
+                if (buffer[k] == ' ')
+                        str_ct++;
+        }
+        str_ct++;
+
+        array = (char **)malloc(sizeof(char *) * (str_ct + 1));
+        if (array == NULL)
+        {
+                perror("tokenize malloc array failure");
+                return (NULL);
+        }
+
+        portion = strtok(buffer, delimiter);
+        while (portion != NULL)
+        {
+                array[i] = strdup(portion);
+                if (array[i] == NULL)
+                {
+                        perror("tokenize strdup array failure]");
+                        for (l = 0; l < i; l++)
+                                free(array[l]);
+                        free(array);
+                        return (NULL);
+                }
+                i++;
+                portion = strtok(NULL, delimiter);
+        }
+        array[i] = NULL;
+        return (array);
+}
