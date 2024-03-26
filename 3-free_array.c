478f71938e4ac675aceded2c4f23b665e5f35a79
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-26 16:01:32 +0000

    created free_array function, added documentation. Added calls to new function in forkcecute and tokenize.

---
+++ 3-free_array.c
@@ -0,0 +1,21 @@
+#include "main_shell.h"
+
+/*
+ * free_array- liberates memory for dynamically allocated arrays of strings.
+ *
+ * @i: an integer used to iterate through the array.
+ *
+ * Return: nothing.
+ */
+
+void free_array(char **array)
+{
+    int i = 0;
+
+    while (array[i] != NULL)
+    {
+        free(array[i]);
+        i++;
+    }
+    free(array);
+}
